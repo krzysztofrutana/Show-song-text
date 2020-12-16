201d52581a5520ab91e2e7cd371be76885c7d938
Krzysztof Rutana <krzysztofrutana@wp.pl>
2020-12-16 21:28:08 +0100

    Add radio button round line in interface color (needed custom renderer). Add custom view with rounded border corner (thanks for  Venkata Swamy Balaraju).

---
+++ Show song text/Show song text.Android/CustomRenderer/CustomRadioButtonRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Show_song_text.Droid.CustomRenderer;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.RadioButton), typeof(RadioButtonCustomRenderer))]
+namespace Show_song_text.Droid.CustomRenderer
+{
+    public class RadioButtonCustomRenderer : RadioButtonRenderer
+    {
+        public RadioButtonCustomRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.ButtonTintList = ColorStateList.ValueOf(Android.Graphics.Color.ParseColor("#FE1A1A"));
+            }
+        }
+    }
+}
---
+++ Show song text/Show song text.Android/CustomRenderer/RoundedCornerViewRenderer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Show_song_text.CustomControls;
+using Show_song_text.Droid.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+/*
+ *Thanks for Venkata Swamy Balaraju for this solution
+* WWW: https://www.c-sharpcorner.com/article/how-to-set-corner-radius-for-view-layout-cell-grid-stack-listview-in-xamar/
+*/
+[assembly: ExportRenderer(typeof(RoundedCornerView), typeof(RoundedCornerViewRenderer))]
+namespace Show_song_text.Droid.CustomRenderer
+{
+    public class RoundedCornerViewRenderer : ViewRenderer
+    {
+
+
+        public RoundedCornerViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+        {
+            base.OnElementChanged(e);
+        }
+        protected override bool DrawChild(Canvas canvas, global::Android.Views.View child, long drawingTime)
+        {
+            if (Element == null) return false;
+            RoundedCornerView rcv = (RoundedCornerView)Element;
+            this.SetClipChildren(true);
+            rcv.Padding = new Thickness(0, 0, 0, 0);
+            //rcv.HasShadow = false;      
+            int radius = (int)(rcv.RoundedCornerRadius);
+            // Check if make circle is set to true. If so, then we just use the width and      
+            // height of the control to calculate the radius. RoundedCornerRadius will be ignored      
+            // in this case.      
+            if (rcv.MakeCircle)
+            {
+                radius = Math.Min(Width, Height) / 2;
+            }
+            // When we create a round rect, we will have to double the radius since it is not      
+            // the same as creating a circle.      
+            radius *= 2;
+            try
+            {
+                //Create path to clip the child       
+                var path = new Path();
+                path.AddRoundRect(new RectF(0, 0, Width, Height), new float[] {
+                    radius,
+                    radius,
+                    radius,
+                    radius,
+                    radius,
+                    radius,
+                    radius,
+                    radius
+                }, Path.Direction.Ccw);
+                canvas.Save();
+                canvas.ClipPath(path);
+                // Draw the child first so that the border shows up above it.      
+                var result = base.DrawChild(canvas, child, drawingTime);
+                canvas.Restore();
+                /*   
+                 * If a border is specified, we use the same path created above to stroke   
+                 * with the border color.   
+                 * */
+                if (rcv.BorderWidth > 0)
+                {
+                    // Draw a filled circle.      
+                    var paint = new Paint();
+                    paint.AntiAlias = true;
+                    paint.StrokeWidth = rcv.BorderWidth;
+                    paint.SetStyle(Paint.Style.Stroke);
+                    paint.Color = rcv.BorderColor.ToAndroid();
+                    canvas.DrawPath(path, paint);
+                    paint.Dispose();
+                }
+                //Properly dispose      
+                path.Dispose();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                System.Console.Write(ex.Message);
+            }
+            return base.DrawChild(canvas, child, drawingTime);
+        }
+    }
+}
