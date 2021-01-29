4a347ecf10e05378df342773874e1ed6200ac33c
Krzysztof Rutana <krzysztofrutana@wp.pl>
2021-01-29 10:34:26 +0100

    Add log implementation in android using Dependency Service.

---
+++ Show song text/Show song text.Android/LogImplementation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+using Show_song_text.Interfaces;
+using Show_song_text.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(LogImplementation))]
+namespace Show_song_text.Droid
+{
+    public class LogImplementation : ILogInterface
+    {
+        public void Debug(string TAG, string message)
+        {
+            Log.Debug(TAG, message);
+        }
+
+        public void Error(string TAG, string message)
+        {
+            Log.Error(TAG, message);
+        }
+
+        public void Info(string TAG, string message)
+        {
+            Log.Info(TAG, message);
+        }
+
+        public void Verbose(string TAG, string message)
+        {
+            Log.Verbose(TAG, message);
+        }
+
+        public void Warn(string TAG, string message)
+        {
+            Log.Warn(TAG, message);
+        }
+    }
+}
